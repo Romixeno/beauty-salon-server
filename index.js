3d6b6f11434242fa5cf7d56e535d8871a1b55bc9
Raoera Romino <Romixeno@gmail.com>
2024-02-29 18:10:30 +0300

    origin

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://192.168.137.1:8080",
+    origin: "http://127.0.0.1:8080",
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
     credentials: true,
   })
