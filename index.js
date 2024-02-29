9719fefc9564285630136c3022ae703c6deb62ab
Raoera Romino <Romixeno@gmail.com>
2024-02-29 19:05:58 +0300

    ip change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://192.168.137.1:3000",
+    origin: "https://salonbeauty.netlify.app",
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
     credentials: true,
   })
